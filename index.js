d3e2db0373482aa44451a7ef31903abb7e586bcd
Nahid Sultan <nahidsultan38@gmail.com>
2024-06-04 19:44:44 +0800

    .env file edited

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://nahid:E1K8EVTulaURuniB@cluster0.tyhvkrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tyhvkrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
